fb111408951f8c967ab90bcd882e719928e846bc
yellowsunn <chohankook95@gmail.com>
2023-10-08 17:14:49 +0900

    모듈 디렉토리 변경

--- sonarqube-pr-decoration/build.gradle.kts
+++ sonarqube-pr-decoration/build.gradle.kts
@@ -1,32 +1,12 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management") version "1.1.3"
-    id("org.sonarqube") version "3.5.0.2730"
-}
-
 group = "com.yellowsunn"
 version = "0.0.1-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-    mavenCentral()
+plugins {
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "github.pr.project")
-    }
 }
